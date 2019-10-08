ca17faf5cee6a904fa99f41d12ae6bfa41bd2f24
Łukasz Niemier <lukasz@niemier.pl>
2019-10-08 14:13:20 +0200

    Install dependencies before running tasks

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,5 @@ cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-mix local.rebar --force
-mix local.hex --force
-mix deps.get
-
 mix credo suggest --strict --format=flycheck \
     | reviewdog -efm="%f:%l:%c: %t: %m" -efm="%f:%l: %t: %m" -name="credo" -reporter="${INPUT_REPORTER:-'github-pr-check'}" -level="${INPUT_LEVEL}"
