dd31d540def3911eb1cab9a2ba27d3ef65de8056
David Webster <davidcwebster@gmail.com>
2021-09-29 22:36:15 +1000

    Fix error "** (Mix) Could not find an SCM for dependency :credo from MyApp.MixProject"

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+export MIX_HOME=/var/mix
 
 mix credo suggest --strict --format=flycheck \
     | reviewdog -efm="%f:%l:%c: %t: %m" -efm="%f:%l: %t: %m" -name="credo" -reporter="${INPUT_REPORTER:-'github-pr-check'}" -level="${INPUT_LEVEL}"
