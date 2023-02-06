49b679b56cfa29e9cedd29a0c132c5395650fe02
k-asm <asami.kosuke@gmail.com>
2023-02-06 11:55:48 +0900

    Fix git error introduced in git 2.35.2

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 cd "$GITHUB_WORKSPACE"
 
+git config --global --add safe.directory $GITHUB_WORKSPACE || exit 1
+
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 mix credo suggest --strict --format=flycheck \
